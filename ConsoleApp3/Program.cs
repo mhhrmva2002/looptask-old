b2ff17f3e8699609b30cff0be45039dff84e6433
mhhtmva2002 <ilahamaharramovaa@gmail.com>
2023-02-28 00:31:56 +0400

    csharp part

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double reqem = 0;
+            double tersreqem = 0;
+            double  qaliq = 0;
+            while ( reqem > 0)
+            {
+               if ( reqem == tersreqem) 
+                {
+                    qaliq = reqem % 10;
+                    reqem = (reqem - qaliq) / 10;
+                    tersreqem = tersreqem * 10 + qaliq;
+                }
+                
+            }
+            Console.WriteLine(true);
+        
+
+
+
+        }
+    }
+}
